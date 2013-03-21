cf34a485f603a72675a92029634d927bc805c842
Richard da Silva <richard_dasilva@hotmail.com>
2013-03-21 01:14:19 +0000

    Finnish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1' , :text => 'Sample App')
+    end
+    it "should have the title 'Home'" do
+      visit 'static_pages/home'
+      page.should have_selector('title', :text => 'Ruby On Rails Tutorial Sample App | Home')
+    end
+  end
+end
