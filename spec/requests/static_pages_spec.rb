882b8c9a5d325b2c42ed8e37815cc3761177b3f2
Richard da Silva <richard_dasilva@hotmail.com>
2013-03-23 22:02:25 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1' , :text => 'Sample App')
-    end
-    it "should have the title'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => " | Home")
-    end
+
+    before { visit root_path }
+
+    it { should have_selector('h1' , :text => 'Sample App') }
+    it { should have_selector('title', :text => full_title('')) }
+    it { should_not have_selector('title', :text => " | Home") }
+
   end
 end
